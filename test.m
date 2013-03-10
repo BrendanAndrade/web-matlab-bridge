bd7c47286b74eaaa70fdc4e96ffb79ebd2ce64da
Brendan Andrade <brnd4n@gmail.com>
2013-03-10 12:36:50 -0400

    Removed docs and fixed import of java class in test.m

--- test.m
+++ test.m
@@ -1,5 +1,6 @@
-javaaddpath('/home/bandrade/matlab-websocket/Java-WebSocket/dist/');
+javaaddpath('/home/bandrade/matlab-websocket/Java-WebSocket/dist/java_websocket.jar');
 import java.net.URI
+import ROSBridgeClient.*
 
 MASTER_URI = URI('ws://localhost:9090');
 
@@ -8,7 +9,7 @@ client = ROSBridgeClient(MASTER_URI);
 
 client.connect();
 pause(0.10);
-sub_top = '{"op": "subscribe", "topic": "/waypoint_input", "type": "geometry_msgs/PoseArray"}';
+sub_top = '{"op": "subscribe", "topic": "/test_topic", "type": "std_msgs/Int16"}';
 client.send(sub_top);
 pause
 client.close()
