f00340a5d24828d58d1098b189fabeab488d99f9
Brendan Andrade <brnd4n@gmail.com>
2013-03-19 18:05:58 -0400

    Implemented callback function within class for incoming messages

--- ROSBridge.m
+++ ROSBridge.m
@@ -35,6 +35,10 @@ classdef ROSBridge
                 pause(0.10)
                 retry = retry + 1;
             end
+            
+            % Set callback for incoming message
+            set(obj.client, 'MessageReceivedCallback', @(h,e) obj.message_callback() )
+            
         end
         
         function delete(obj)
@@ -43,7 +47,7 @@ classdef ROSBridge
             end
         end
         
-        function send(message)
+        function send(obj, message)
             obj.client.send(message)
         end
         
@@ -51,6 +55,32 @@ classdef ROSBridge
             obj.client.close()
         end
             
+        function message_struct = json_to_struct(message)
+            message_struct = loadjson(char(message));
+        end
+         h, e
+        function subscribe(obj, name, type, callback)
+            message = strcat('{"op": "subscribe", "topic": "', name, '", "type": "', type, '"}');
+            obj.send(message);
+        end
+        
+        function advertise(obj, name, type)
+            message = strcat('{"op": "advertise", "topic": "', name, '", "type": "', type, '"}');
+            obj.send(message);
+        end
+        
+        function unadvertise(obj, name)
+            message = strcat('{"op": "unadvertise", "topic": "', name, '"}');
+            obj.send(message);
+        end
+        
+        function message_callback(obj)
+            disp(obj.client.message);
+            % message_struct = obj.loadjson(obj.client.message);
+            
+        end
+            
+        
     end
     
 end
