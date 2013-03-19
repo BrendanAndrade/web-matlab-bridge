0fb65c142019bfa16c5d78b5ca10e7a56078daa5
Brendan Andrade <brnd4n@gmail.com>
2013-03-19 18:06:57 -0400

    Adding open source jsonlab matlab library.

---
+++ Publisher.m
@@ -0,0 +1,18 @@
+classdef Publisher
+    %UNTITLED2 Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        topic
+        type
+        data
+    end
+    
+    methods
+        
+        
+        
+    end
+    
+end
+
