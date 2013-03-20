d1b829b478117710e229c5030a4bba442556f5d1
Brendan Andrade <brnd4n@gmail.com>
2013-03-20 00:56:49 -0400

    Changed test to test pose array subscriber

--- test.m
+++ test.m
@@ -1,5 +1,29 @@
-cb = @(data) disp(data);
+classdef test
 
-ws = ros_websocket('ws://localhost:9090');
-sub = Subscriber(ws, 'test_topic', 'std_msgs/Int16', cb);
-pub = Publisher(ws, 'test_pub', 'std_msgs/Int16');
+    properties
+        ws
+        sub
+        pub
+        message
+    end
+    
+    methods
+        function obj = test
+            obj.ws = ros_websocket('ws://localhost:9090');
+            obj.sub = Subscriber(obj.ws, 'test_topic', 'geometry_msgs/PoseArray', @obj.callback);
+            obj.pub = Publisher(obj.ws, 'test_pub', 'std_msgs/Int16');
+        end
+        
+        function obj = callback(obj,data)
+            obj.message = data;
+            disp(obj.message.poses.position.x);
+        end
+    end
+end
+
+
+%{
+
+rostopic pub --once /test_topic geometry_msgs/PoseArray '{poses: [position: {x: 0.1, y: 0.1, z: 0.0}]}'
+
+%}
