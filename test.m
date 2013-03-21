6417f48d08980ed25547c3616869bef6dd6d33eb
Brendan Andrade <brnd4n@gmail.com>
2013-03-21 17:21:26 -0400

    Added demo of how to store access subscriber data

--- test.m
+++ test.m
@@ -4,19 +4,22 @@ classdef test
         ws
         sub
         pub
-        message
     end
     
     methods
         function obj = test
             obj.ws = ros_websocket('ws://localhost:9090');
-            obj.sub = Subscriber(obj.ws, 'test_topic', 'geometry_msgs/PoseArray', @obj.callback);
+            obj.sub = Subscriber(obj.ws, 'test_topic', 'geometry_msgs/PoseArray');
+            addlistener(obj.sub, 'OnMessageReceived', @(h,e) obj.callback);
             obj.pub = Publisher(obj.ws, 'test_pub', 'std_msgs/Int16');
         end
         
-        function obj = callback(obj,data)
-            obj.message = data;
-            disp(obj.message.poses.position.x);
+        function obj = callback(obj)          
+            disp(obj.sub.data.poses.position.x);
+        end
+        
+        function out = data(obj)
+            out = obj.sub.data;
         end
     end
 end
@@ -25,6 +28,6 @@ end
 %{
 
 rostopic pub --once /test_topic geometry_msgs/PoseArray '{poses: [position:
-{x: 0.1, y: 0.1, z: 0.0}]}'git 
+{x: 0.1, y: 0.1, z: 0.0}]}' 
 
 %}
